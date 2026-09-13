292325f3d5c17f889d76443682f39899cea519b0
The-Noctura <muhamadalghani584@gmail.com>
2026-09-14 04:25:07 +0700

    refactor: Add status selection to mapel creation form

--- resources/views/admin/mapel/create.blade.php
+++ resources/views/admin/mapel/create.blade.php
@@ -9,6 +9,7 @@
 
             <x-form-input name="nama_mapel" label="Nama Mapel" :value="old('nama_mapel')" />
             <x-form-input name="kode_mapel" label="Kode Mapel" :value="old('kode_mapel')" />
+            <x-form-select name="status" label="Status" :options="['aktif' => 'Aktif', 'nonaktif' => 'Nonaktif']" :value="old('status', 'aktif')" />
 
             <div class="flex justify-end gap-3 mt-6">
                 <x-button variant="secondary" type="button" onclick="history.back()">Batal</x-button>
